ccf3a4c912ab0be7e5eae73d1d2bc14f9dc29f48
lizhuo-1994 <joeywoton@gmail.com>
2023-09-08 03:51:49 +0900

    update

--- scripts/train_bullet_baseline.sh
+++ scripts/train_bullet_baseline.sh
@@ -3,116 +3,105 @@
 # SafetyDroneCircle-v0
 # SafetyCarCircle-v0
 
-# SafetyHalfCheetahVelocityGymnasium-v1
-# SafetyHopperVelocityGymnasium-v1
-# SafetyWalker2dVelocityGymnasium-v1
-# SafetyAntVelocityGymnasium-v1
-
-# SafetyPointButton1Gymnasium-v0
-# SafetyCarGoal1Gymnasium-v0
-# SafetyPointPush1Gymnasium-v0
-# SafetyPointPush2Gymnasium-v0
-
-
 # SafetyBallCircle-v0 
-# python  train_cpo_agent.py --task SafetyBallCircle-v0
-# python  train_cpo_agent.py --task SafetyBallCircle-v0
-# python  train_cpo_agent.py --task SafetyBallCircle-v0
+python  train_cpo_agent.py --task SafetyBallCircle-v0 --epoch 50
+python  train_cpo_agent.py --task SafetyBallCircle-v0 --epoch 50
+python  train_cpo_agent.py --task SafetyBallCircle-v0 --epoch 50
 
-# python  train_cvpo_agent.py --task SafetyBallCircle-v0
-# python  train_cvpo_agent.py --task SafetyBallCircle-v0
-# python  train_cvpo_agent.py --task SafetyBallCircle-v0
+python  train_cvpo_agent.py --task SafetyBallCircle-v0 --epoch 50
+python  train_cvpo_agent.py --task SafetyBallCircle-v0 --epoch 50
+python  train_cvpo_agent.py --task SafetyBallCircle-v0
 
-# python  train_ddpgl_agent.py --task SafetyBallCircle-v0
-# python  train_ddpgl_agent.py --task SafetyBallCircle-v0
-# python  train_ddpgl_agent.py --task SafetyBallCircle-v0
+python  train_ddpgl_agent.py --task SafetyBallCircle-v0 --epoch 50
+python  train_ddpgl_agent.py --task SafetyBallCircle-v0 --epoch 50
+python  train_ddpgl_agent.py --task SafetyBallCircle-v0 --epoch 50
 
-# python  train_focops_agent.py --task SafetyBallCircle-v0
-# python  train_focops_agent.py --task SafetyBallCircle-v0
-# python  train_focops_agent.py --task SafetyBallCircle-v0
+python  train_focops_agent.py --task SafetyBallCircle-v0 --epoch 50
+python  train_focops_agent.py --task SafetyBallCircle-v0 --epoch 50
+python  train_focops_agent.py --task SafetyBallCircle-v0 --epoch 50
 
-# python  train_ppol_agent.py --task SafetyBallCircle-v0
-# python  train_ppol_agent.py --task SafetyBallCircle-v0
-# python  train_ppol_agent.py --task SafetyBallCircle-v0
+python  train_ppol_agent.py --task SafetyBallCircle-v0 --epoch 50
+python  train_ppol_agent.py --task SafetyBallCircle-v0 --epoch 50
+python  train_ppol_agent.py --task SafetyBallCircle-v0 --epoch 50
 
-# python  train_trpol_agent.py --task SafetyBallCircle-v0
-# python  train_trpol_agent.py --task SafetyBallCircle-v0
-# python  train_trpol_agent.py --task SafetyBallCircle-v0
+python  train_trpol_agent.py --task SafetyBallCircle-v0 --epoch 50
+python  train_trpol_agent.py --task SafetyBallCircle-v0 --epoch 50
+python  train_trpol_agent.py --task SafetyBallCircle-v0 --epoch 50
 
 
 # SafetyAntRun-v0
-# python  train_cpo_agent.py --task SafetyAntRun-v0
-# python  train_cpo_agent.py --task SafetyAntRun-v0
-# python  train_cpo_agent.py --task SafetyAntRun-v0
+python  train_cpo_agent.py --task SafetyAntRun-v0 --epoch 50
+python  train_cpo_agent.py --task SafetyAntRun-v0 --epoch 50
+python  train_cpo_agent.py --task SafetyAntRun-v0 --epoch 50
 
-# python  train_cvpo_agent.py --task SafetyAntRun-v0
-# python  train_cvpo_agent.py --task SafetyAntRun-v0
-# python  train_cvpo_agent.py --task SafetyAntRun-v0
+python  train_cvpo_agent.py --task SafetyAntRun-v0 --epoch 50
+python  train_cvpo_agent.py --task SafetyAntRun-v0 --epoch 50
+python  train_cvpo_agent.py --task SafetyAntRun-v0 --epoch 50
 
-# python  train_ddpgl_agent.py --task SafetyAntRun-v0
-# python  train_ddpgl_agent.py --task SafetyAntRun-v0
-# python  train_ddpgl_agent.py --task SafetyAntRun-v0
+python  train_ddpgl_agent.py --task SafetyAntRun-v0 --epoch 50
+python  train_ddpgl_agent.py --task SafetyAntRun-v0 --epoch 50
+python  train_ddpgl_agent.py --task SafetyAntRun-v0 --epoch 50
 
-# python  train_focops_agent.py --task SafetyAntRun-v0
-# python  train_focops_agent.py --task SafetyAntRun-v0
-# python  train_focops_agent.py --task SafetyAntRun-v0
+python  train_focops_agent.py --task SafetyAntRun-v0 --epoch 50
+python  train_focops_agent.py --task SafetyAntRun-v0 --epoch 50
+python  train_focops_agent.py --task SafetyAntRun-v0 --epoch 50
 
-# python  train_ppol_agent.py --task SafetyAntRun-v0
-# python  train_ppol_agent.py --task SafetyAntRun-v0
-# python  train_ppol_agent.py --task SafetyAntRun-v0
+python  train_ppol_agent.py --task SafetyAntRun-v0 --epoch 50
+python  train_ppol_agent.py --task SafetyAntRun-v0 --epoch 50
+python  train_ppol_agent.py --task SafetyAntRun-v0 --epoch 50
 
-# python  train_trpol_agent.py --task SafetyAntRun-v0
-# python  train_trpol_agent.py --task SafetyAntRun-v0
-# python  train_trpol_agent.py --task SafetyAntRun-v0
+python  train_trpol_agent.py --task SafetyAntRun-v0 --epoch 50
+python  train_trpol_agent.py --task SafetyAntRun-v0 --epoch 50
+python  train_trpol_agent.py --task SafetyAntRun-v0 --epoch 50
 
 
 # SafetyDroneCircle-v0
-# python  train_cpo_agent.py --task SafetyDroneCircle-v0
-# python  train_cpo_agent.py --task SafetyDroneCircle-v0
-# python  train_cpo_agent.py --task SafetyDroneCircle-v0
+python  train_cpo_agent.py --task SafetyDroneCircle-v0 --epoch 50
+python  train_cpo_agent.py --task SafetyDroneCircle-v0 --epoch 50
+python  train_cpo_agent.py --task SafetyDroneCircle-v0 --epoch 50
 
-# python  train_cvpo_agent.py --task SafetyDroneCircle-v0
-# python  train_cvpo_agent.py --task SafetyDroneCircle-v0
-# python  train_cvpo_agent.py --task SafetyDroneCircle-v0
+python  train_cvpo_agent.py --task SafetyDroneCircle-v0 --epoch 50
+python  train_cvpo_agent.py --task SafetyDroneCircle-v0 --epoch 50
+python  train_cvpo_agent.py --task SafetyDroneCircle-v0 --epoch 50
 
-# python  train_ddpgl_agent.py --task SafetyDroneCircle-v0
-# python  train_ddpgl_agent.py --task SafetyDroneCircle-v0
-# python  train_ddpgl_agent.py --task SafetyDroneCircle-v0
+python  train_ddpgl_agent.py --task SafetyDroneCircle-v0 --epoch 50
+python  train_ddpgl_agent.py --task SafetyDroneCircle-v0 --epoch 50
+python  train_ddpgl_agent.py --task SafetyDroneCircle-v0 --epoch 50
 
-# python  train_focops_agent.py --task SafetyDroneCircle-v0
-# python  train_focops_agent.py --task SafetyDroneCircle-v0
-# python  train_focops_agent.py --task SafetyDroneCircle-v0
+python  train_focops_agent.py --task SafetyDroneCircle-v0 --epoch 50
+python  train_focops_agent.py --task SafetyDroneCircle-v0 --epoch 50
+python  train_focops_agent.py --task SafetyDroneCircle-v0 --epoch 50
 
-# python  train_ppol_agent.py --task SafetyDroneCircle-v0
-# python  train_ppol_agent.py --task SafetyDroneCircle-v0
-# python  train_ppol_agent.py --task SafetyDroneCircle-v0
+python  train_ppol_agent.py --task SafetyDroneCircle-v0 --epoch 50
+python  train_ppol_agent.py --task SafetyDroneCircle-v0 --epoch 50
+python  train_ppol_agent.py --task SafetyDroneCircle-v0 --epoch 50
 
-# python  train_trpol_agent.py --task SafetyDroneCircle-v0
-# python  train_trpol_agent.py --task SafetyDroneCircle-v0
-# python  train_trpol_agent.py --task SafetyDroneCircle-v0
+python  train_trpol_agent.py --task SafetyDroneCircle-v0 --epoch 50
+python  train_trpol_agent.py --task SafetyDroneCircle-v0 --epoch 50
+python  train_trpol_agent.py --task SafetyDroneCircle-v0 --epoch 50
 
 
 # SafetyCarCircle-v0
-# python  train_cpo_agent.py --task SafetyCarCircle-v0
-# python  train_cpo_agent.py --task SafetyCarCircle-v0
-# python  train_cpo_agent.py --task SafetyCarCircle-v0
+python  train_cpo_agent.py --task SafetyCarCircle-v0 --epoch 50
+python  train_cpo_agent.py --task SafetyCarCircle-v0 --epoch 50
+python  train_cpo_agent.py --task SafetyCarCircle-v0 --epoch 50
 
-# python  train_cvpo_agent.py --task SafetyCarCircle-v0
-# python  train_cvpo_agent.py --task SafetyCarCircle-v0
-# python  train_cvpo_agent.py --task SafetyCarCircle-v0
+python  train_cvpo_agent.py --task SafetyCarCircle-v0 --epoch 50
+python  train_cvpo_agent.py --task SafetyCarCircle-v0 --epoch 50
+python  train_cvpo_agent.py --task SafetyCarCircle-v0 --epoch 50
 
-# python  train_ddpgl_agent.py --task SafetyCarCircle-v0
-# python  train_ddpgl_agent.py --task SafetyCarCircle-v0
-# python  train_ddpgl_agent.py --task SafetyCarCircle-v0
+python  train_ddpgl_agent.py --task SafetyCarCircle-v0 --epoch 50
+python  train_ddpgl_agent.py --task SafetyCarCircle-v0 --epoch 50
+python  train_ddpgl_agent.py --task SafetyCarCircle-v0 --epoch 50
 
-# python  train_focops_agent.py --task SafetyCarCircle-v0
-# python  train_focops_agent.py --task SafetyCarCircle-v0
-# python  train_focops_agent.py --task SafetyCarCircle-v0
+python  train_focops_agent.py --task SafetyCarCircle-v0 --epoch 50
+python  train_focops_agent.py --task SafetyCarCircle-v0 --epoch 50
+python  train_focops_agent.py --task SafetyCarCircle-v0 --epoch 50
 
-# python  train_ppol_agent.py --task SafetyCarCircle-v0
-# python  train_ppol_agent.py --task SafetyCarCircle-v0
-# python  train_ppol_agent.py --task SafetyCarCircle-v0
+python  train_ppol_agent.py --task SafetyCarCircle-v0 --epoch 50
+python  train_ppol_agent.py --task SafetyCarCircle-v0 --epoch 50
+python  train_ppol_agent.py --task SafetyCarCircle-v0 --epoch 50
 
-# python  train_trpol_agent.py --task SafetyCarCircle-v0
-# python  train_trpol_agent.py --task SafetyCarCircle-v0
-# python  train_trpol_agent.py --task SafetyCarCircle-v0
+python  train_trpol_agent.py --task SafetyCarCircle-v0 --epoch 50
+python  train_trpol_agent.py --task SafetyCarCircle-v0 --epoch 50
+python  train_trpol_agent.py --task SafetyCarCircle-v0 --epoch 50
