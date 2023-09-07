be3dc181dfc4bb5bf95b70c38bac6c8e47a20832
lizhuo-1994 <joeywoton@gmail.com>
2023-09-08 03:18:07 +0900

    update

---
+++ scripts/train_navigation_baseline.sh
@@ -0,0 +1,107 @@
+# SafetyPointButton1Gymnasium-v0
+# SafetyCarGoal1Gymnasium-v0
+# SafetyPointPush1Gymnasium-v0
+# SafetyPointPush2Gymnasium-v0
+
+# SafetyPointButton1Gymnasium-v0 
+python  train_cpo_agent.py --task SafetyPointButton1Gymnasium-v0 --epoch 200
+python  train_cpo_agent.py --task SafetyPointButton1Gymnasium-v0 --epoch 200
+python  train_cpo_agent.py --task SafetyPointButton1Gymnasium-v0 --epoch 200
+
+python  train_cvpo_agent.py --task SafetyPointButton1Gymnasium-v0 --epoch 200
+python  train_cvpo_agent.py --task SafetyPointButton1Gymnasium-v0 --epoch 200
+python  train_cvpo_agent.py --task SafetyPointButton1Gymnasium-v0 --epoch 200
+
+python  train_ddpgl_agent.py --task SafetyPointButton1Gymnasium-v0 --epoch 200
+python  train_ddpgl_agent.py --task SafetyPointButton1Gymnasium-v0 --epoch 200
+python  train_ddpgl_agent.py --task SafetyPointButton1Gymnasium-v0 --epoch 200
+
+python  train_focops_agent.py --task SafetyPointButton1Gymnasium-v0 --epoch 200
+python  train_focops_agent.py --task SafetyPointButton1Gymnasium-v0 --epoch 200
+python  train_focops_agent.py --task SafetyPointButton1Gymnasium-v0 --epoch 200
+
+python  train_ppol_agent.py --task SafetyPointButton1Gymnasium-v0 --epoch 200
+python  train_ppol_agent.py --task SafetyPointButton1Gymnasium-v0 --epoch 200
+python  train_ppol_agent.py --task SafetyPointButton1Gymnasium-v0 --epoch 200
+
+python  train_trpol_agent.py --task SafetyPointButton1Gymnasium-v0 --epoch 200
+python  train_trpol_agent.py --task SafetyPointButton1Gymnasium-v0 --epoch 200
+python  train_trpol_agent.py --task SafetyPointButton1Gymnasium-v0 --epoch 200
+
+
+# SafetyCarGoal1Gymnasium-v0
+python  train_cpo_agent.py --task SafetyCarGoal1Gymnasium-v0 --epoch 200
+python  train_cpo_agent.py --task SafetyCarGoal1Gymnasium-v0 --epoch 200
+python  train_cpo_agent.py --task SafetyCarGoal1Gymnasium-v0 --epoch 200
+
+python  train_cvpo_agent.py --task SafetyCarGoal1Gymnasium-v0 --epoch 200
+python  train_cvpo_agent.py --task SafetyCarGoal1Gymnasium-v0 --epoch 200
+python  train_cvpo_agent.py --task SafetyCarGoal1Gymnasium-v0 --epoch 200
+
+python  train_ddpgl_agent.py --task SafetyCarGoal1Gymnasium-v0 --epoch 200
+python  train_ddpgl_agent.py --task SafetyCarGoal1Gymnasium-v0 --epoch 200
+python  train_ddpgl_agent.py --task SafetyCarGoal1Gymnasium-v0 --epoch 200
+
+python  train_focops_agent.py --task SafetyCarGoal1Gymnasium-v0 --epoch 200
+python  train_focops_agent.py --task SafetyCarGoal1Gymnasium-v0 --epoch 200
+python  train_focops_agent.py --task SafetyCarGoal1Gymnasium-v0 --epoch 200
+
+python  train_ppol_agent.py --task SafetyCarGoal1Gymnasium-v0 --epoch 200
+python  train_ppol_agent.py --task SafetyCarGoal1Gymnasium-v0 --epoch 200
+python  train_ppol_agent.py --task SafetyCarGoal1Gymnasium-v0 --epoch 200
+
+python  train_trpol_agent.py --task SafetyCarGoal1Gymnasium-v0 --epoch 200
+python  train_trpol_agent.py --task SafetyCarGoal1Gymnasium-v0 --epoch 200
+python  train_trpol_agent.py --task SafetyCarGoal1Gymnasium-v0 --epoch 200
+
+
+# SafetyPointPush1Gymnasium-v0
+python  train_cpo_agent.py --task SafetyPointPush1Gymnasium-v0 --epoch 200
+python  train_cpo_agent.py --task SafetyPointPush1Gymnasium-v0 --epoch 200
+python  train_cpo_agent.py --task SafetyPointPush1Gymnasium-v0 --epoch 200
+
+python  train_cvpo_agent.py --task SafetyPointPush1Gymnasium-v0 --epoch 200
+python  train_cvpo_agent.py --task SafetyPointPush1Gymnasium-v0 --epoch 200
+python  train_cvpo_agent.py --task SafetyPointPush1Gymnasium-v0 --epoch 200
+
+python  train_ddpgl_agent.py --task SafetyPointPush1Gymnasium-v0 --epoch 200
+python  train_ddpgl_agent.py --task SafetyPointPush1Gymnasium-v0 --epoch 200
+python  train_ddpgl_agent.py --task SafetyPointPush1Gymnasium-v0 --epoch 200
+
+python  train_focops_agent.py --task SafetyPointPush1Gymnasium-v0 --epoch 200
+python  train_focops_agent.py --task SafetyPointPush1Gymnasium-v0 --epoch 200
+python  train_focops_agent.py --task SafetyPointPush1Gymnasium-v0 --epoch 200
+
+python  train_ppol_agent.py --task SafetyPointPush1Gymnasium-v0 --epoch 200
+python  train_ppol_agent.py --task SafetyPointPush1Gymnasium-v0 --epoch 200
+python  train_ppol_agent.py --task SafetyPointPush1Gymnasium-v0 --epoch 200
+
+python  train_trpol_agent.py --task SafetyPointPush1Gymnasium-v0 --epoch 200
+python  train_trpol_agent.py --task SafetyPointPush1Gymnasium-v0 --epoch 200
+python  train_trpol_agent.py --task SafetyPointPush1Gymnasium-v0 --epoch 200
+
+
+# # SafetyPointPush2Gymnasium-v0
+python  train_cpo_agent.py --task SafetyPointPush2Gymnasium-v0 --epoch 200
+python  train_cpo_agent.py --task SafetyPointPush2Gymnasium-v0 --epoch 200
+python  train_cpo_agent.py --task SafetyPointPush2Gymnasium-v0 --epoch 200
+
+python  train_cvpo_agent.py --task SafetyPointPush2Gymnasium-v0 --epoch 200
+python  train_cvpo_agent.py --task SafetyPointPush2Gymnasium-v0 --epoch 200
+python  train_cvpo_agent.py --task SafetyPointPush2Gymnasium-v0 --epoch 200
+
+python  train_ddpgl_agent.py --task SafetyPointPush2Gymnasium-v0 --epoch 200
+python  train_ddpgl_agent.py --task SafetyPointPush2Gymnasium-v0 --epoch 200
+python  train_ddpgl_agent.py --task SafetyPointPush2Gymnasium-v0 --epoch 200
+
+python  train_focops_agent.py --task SafetyPointPush2Gymnasium-v0 --epoch 200
+python  train_focops_agent.py --task SafetyPointPush2Gymnasium-v0 --epoch 200
+python  train_focops_agent.py --task SafetyPointPush2Gymnasium-v0 --epoch 200
+
+python  train_ppol_agent.py --task SafetyPointPush2Gymnasium-v0 --epoch 200
+python  train_ppol_agent.py --task SafetyPointPush2Gymnasium-v0 --epoch 200
+python  train_ppol_agent.py --task SafetyPointPush2Gymnasium-v0 --epoch 200
+
+python  train_trpol_agent.py --task SafetyPointPush2Gymnasium-v0 --epoch 200
+python  train_trpol_agent.py --task SafetyPointPush2Gymnasium-v0 --epoch 200
+python  train_trpol_agent.py --task SafetyPointPush2Gymnasium-v0 --epoch 200
---
+++ scripts/train_navigation_episodic.sh
@@ -0,0 +1,40 @@
+# SafetyPointButton1Gymnasium-v0
+# SafetyCarGoal1Gymnasium-v0
+# SafetyPointPush1Gymnasium-v0
+# SafetyPointPush2Gymnasium-v0
+
+
+# SafetyPointButton1Gymnasium-v0 
+python  train_ddpgl_agent.py --task SafetyPointButton1Gymnasium-v0 --epoch 200  --episodic True --episodic_step 3 
+pkill -f SafetyPointButton1Gymnasium
+python  train_ddpgl_agent.py --task SafetyPointButton1Gymnasium-v0 --epoch 200  --episodic True --episodic_step 3 
+pkill -f SafetyPointButton1Gymnasium
+python  train_ddpgl_agent.py --task SafetyPointButton1Gymnasium-v0 --epoch 200  --episodic True --episodic_step 3 
+pkill -f SafetyPointButton1Gymnasium
+
+
+# SafetyCarGoal1Gymnasium-v0 
+python  train_ddpgl_agent.py --task SafetyCarGoal1Gymnasium-v0 --epoch 200  --episodic True --episodic_step 3 
+pkill -f SafetyCarGoal1Gymnasium
+python  train_ddpgl_agent.py --task SafetyCarGoal1Gymnasium-v0 --epoch 200  --episodic True --episodic_step 3 
+pkill -f SafetyCarGoal1Gymnasium
+python  train_ddpgl_agent.py --task SafetyCarGoal1Gymnasium-v0 --epoch 200  --episodic True --episodic_step 3 
+pkill -f SafetyCarGoal1Gymnasium
+
+
+# SafetyPointPush1Gymnasium-v0 
+python  train_ddpgl_agent.py --task SafetyPointPush1Gymnasium-v0 --epoch 200  --episodic True --episodic_step 3 
+pkill -f SafetyPointPush1Gymnasium
+python  train_ddpgl_agent.py --task SafetyPointPush1Gymnasium-v0 --epoch 200  --episodic True --episodic_step 3 
+pkill -f SafetyPointPush1Gymnasium
+python  train_ddpgl_agent.py --task SafetyPointPush1Gymnasium-v0 --epoch 200  --episodic True --episodic_step 3 
+pkill -f SafetyPointPush1Gymnasium
+
+
+# SafetyPointPush2Gymnasium-v0 
+python  train_ddpgl_agent.py --task SafetyPointPush2Gymnasium-v0 --epoch 200  --episodic True --episodic_step 3 
+pkill -f SafetyPointPush2Gymnasium
+python  train_ddpgl_agent.py --task SafetyPointPush2Gymnasium-v0 --epoch 200  --episodic True --episodic_step 3 
+pkill -f SafetyPointPush2Gymnasium
+python  train_ddpgl_agent.py --task SafetyPointPush2Gymnasium-v0 --epoch 200  --episodic True --episodic_step 3 
+pkill -f SafetyPointPush2Gymnasium 
